3f088003efd8fa73c63d0447af453a39c94665a4
fdervaux <francois.dervaux@gmail.com>
2023-02-28 17:11:45 +0100

    update

--- source/main.lua
+++ source/main.lua
@@ -2,10 +2,11 @@ import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
+import "Plugins/AnimatedSprite/AnimatedSprite.lua"
 
 local gfx <const> = playdate.graphics
 
-local playerSprite = nil
+local playerAnimatedSprite = nil
 
 local playerSpeed = 4
 
@@ -14,6 +15,8 @@ local playTime = 30 * 1000
 
 local coinSprite = nil
 local score = 0
+
+local lastDirection = ""
 
 local function resetTimer()
 	playTimer = playdate.timer.new(playTime, playTime, 0, playdate.easingFunctions.linear)
@@ -28,10 +31,26 @@ end
 local function initialize()
 	math.randomseed(playdate.getSecondsSinceEpoch())
 	local playerImage = gfx.image.new("images/player")
-	playerSprite = gfx.sprite.new(playerImage)
-	playerSprite:moveTo(200, 120)
-	playerSprite:setCollideRect(0, 0, playerSprite:getSize())
-	playerSprite:add()
+	local imagetable = playdate.graphics.imagetable.new('images/character-table-32-32.png')
+
+	playerAnimatedSprite = AnimatedSprite.new(imagetable)
+	playerAnimatedSprite:addState('p1IdleUp',1,1,{ tickStep = 10 })
+	playerAnimatedSprite:addState('p1RunUp',2,3,{ tickStep = 10 })
+	
+	playerAnimatedSprite:addState('p1IdleRight',10,10,{ tickStep = 10 })
+	playerAnimatedSprite:addState('p1RunRight',11,12,{ tickStep = 10 })
+	
+	playerAnimatedSprite:addState('p1IdleDown',19,19,{ tickStep = 10 }).asDefault()
+	playerAnimatedSprite:addState('p1RunDown',20,21,{ tickStep = 10 })
+	
+	playerAnimatedSprite:addState('p1IdleLeft',28,28,{ tickStep = 10 })
+	playerAnimatedSprite:addState('p1RunLeft',29,30,{ tickStep = 10 })
+
+	-- playerSprite = gfx.sprite.new(playerImage)
+	playerAnimatedSprite:moveTo(200, 120)
+	playerAnimatedSprite:setCollideRect(8, 8, 16, 16)
+	playerAnimatedSprite:playAnimation()
+
 
 	local coinImage = gfx.image.new("images/coin")
     coinSprite = gfx.sprite.new(coinImage)
@@ -48,12 +67,17 @@ local function initialize()
 		end
 	)
 
+	imagetable = playdate.graphics.imagetable.new('images/Bomberman-Character.png')
+
+	lastDirection = "Down"
+
 	resetTimer()
 end
 
 initialize()
 
 function playdate.update()
+	
 	if playTimer.value == 0 then
 		if playdate.buttonJustPressed(playdate.kButtonA) then
 			resetTimer()
@@ -61,24 +85,39 @@ function playdate.update()
 			score = 0
 		end
 	else
+
 		if playdate.buttonIsPressed(playdate.kButtonUp) then
-			playerSprite:moveBy(0, -playerSpeed)
+			playerAnimatedSprite:forceNextAnimation(true,'p1RunUp')
+			playerAnimatedSprite:moveBy(0, -playerSpeed)
+			lastDirection = "Up"
+		elseif playdate.buttonIsPressed(playdate.kButtonRight) then
+			playerAnimatedSprite:forceNextAnimation(true,'p1RunRight')
+			-- playerAnimatedSprite.changeState('p1runright',true)
+			playerAnimatedSprite:moveBy(playerSpeed, 0)
+			lastDirection = "Right"
+		
+		elseif playdate.buttonIsPressed(playdate.kButtonDown) then
+			playerAnimatedSprite:forceNextAnimation(true,'p1RunDown')
+			playerAnimatedSprite:moveBy(0, playerSpeed)
+			lastDirection = "Down"
+		
+		elseif playdate.buttonIsPressed(playdate.kButtonLeft) then
+			playerAnimatedSprite:forceNextAnimation(true,'p1RunLeft')
+			playerAnimatedSprite:moveBy(-playerSpeed, 0)
+			lastDirection = "Left"
+		else
+			playerAnimatedSprite:forceNextAnimation(true,'p1Idle' .. lastDirection)
 		end
-		if playdate.buttonIsPressed(playdate.kButtonRight) then
-			playerSprite:moveBy(playerSpeed, 0)
-		end
-		if playdate.buttonIsPressed(playdate.kButtonDown) then
-			playerSprite:moveBy(0, playerSpeed)
-		end
-		if playdate.buttonIsPressed(playdate.kButtonLeft) then
-			playerSprite:moveBy(-playerSpeed, 0)
-		end
+
+		-- print(playerAnimatedSprite.currentState)
 
 		local collisions = coinSprite:overlappingSprites()
 		if #collisions >= 1 then
 			moveCoin()
 			score += 1
 		end
+
+		playdate.graphics.sprite.update()
 	end
 
 	playdate.timer.updateTimers()
